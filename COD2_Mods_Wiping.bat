6f41f73a59b73e5027a630f63fd6727b00611970
IB_U_Z_Z_A_R_Dl <62464560+Illegal-Services@users.noreply.github.com>
2021-10-15 11:32:10 +0000

    Add files via upload

--- COD2_Mods_Wiping.bat
+++ COD2_Mods_Wiping.bat
@@ -19,25 +19,21 @@
 ::     @Sintrode - Helped me encoding the CLI.
 ::     A project created in the "server.bat" Discord: https://discord.gg/GSVrHag
 ::------------------------------------------------------------------------------
+cls
 >nul chcp 65001
->nul 2>&1 dism || (
-    echo  ■ This script must be "Run as administrator".
-    echo.
-    <nul set /p= ■ Press {ANY KEY} to exit...
-    >nul pause
-    exit /b 1
-)
-set error=
-set "@ERROR=for %%a in ("!database[%%a]!") do (echo. & echo ERROR: Failed deleting: "%%~nxa". & echo You can still do it manually through Windows file manager. & if not defined error set error=1)"
-Setlocal EnableDelayedExpansion
-cd /d "%~dp0"
 title COD2 Mods Wiping
+>nul 2>&1 dism || call :EXIT 1
+setlocal DisableDelayedExpansion
+cd /d "%~dp0"
+set "@ERROR=for %%A in ("!database[%%A]!") do (echo. & echo ERROR: Failed deleting: "%%~nxA". & echo You can still do it manually through Windows file manager. & if not defined error set error=1)"
+setlocal EnableDelayedExpansion
+set error=
 
 :GET_PATH
 cls
-for /f "tokens=8delims=\" %%a in ('reg query "HKLM\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall" ^| findstr /c:"Call of Duty 2"') do (
-    for /f "tokens=2*" %%b in ('2^>nul reg query "HKLM\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\%%a" /v "InstallLocation"') do (
-        set "InstallLocation=%%c"
+for /f "tokens=8delims=\" %%A in ('reg query "HKLM\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall" ^| findstr /c:"Call of Duty 2"') do (
+    for /f "tokens=2*" %%B in ('2^>nul reg query "HKLM\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\%%A" /v "InstallLocation"') do (
+        set "InstallLocation=%%C"
     )
 )
 call :CHECK_PATH InstallLocation && goto :START
@@ -55,44 +51,45 @@ goto :GET_PATH
 :START
 cls
 echo.
-echo  ■ Searching mods in: "!InstallLocation!"
-echo  ├──────────────────────────────────────
-Setlocal DisableDelayedExpansion
+echo  ■ Searching modded scripts in: "!InstallLocation!"
+echo  ├──────────────────────────────────────────────────────────────────────────────
+setlocal DisableDelayedExpansion
 set first=
 set database[#]=0
-Setlocal EnableDelayedExpansion
+setlocal EnableDelayedExpansion
 set first=1
-for /f "tokens=1*delims=:" %%a in ('2^>nul dir "!InstallLocation!*.iwd" /a:-d /b /s ^| findstr /vrc:"iw_..\.iwd" /c:"localized_.*_iw..\.iwd" ^| findstr /nrc:".*"') do (
+for /f "tokens=1*delims=:" %%A in ('2^>nul dir "!InstallLocation!*.iwd" /a:-d /b /s ^| findstr /vrc:"iw_..\.iwd" /c:"localized_.*_iw..\.iwd" ^| findstr /nrc:".*"') do (
     if defined first endlocal
-    echo  │ %%~fb
-    set "database[%%a]=%%~fb"
-    set "database[#]=%%a"
+    echo  ├ %%~fB
+    set "database[%%A]=%%~fB"
+    set "database[#]=%%A"
 )
-Setlocal EnableDelayedExpansion
-if "!database[#]!"=="0" echo  │ No files found.
-echo  └──────────────────────────────────────
+setlocal EnableDelayedExpansion
+if "!database[#]!"=="0" echo  │ No modded scripts found.
+echo  └──────────────────────────────────────────────────────────────────────────────
 if "!database[#]!"=="0" call :EXIT
 echo.
 <nul set /p=" ■ Do you confirm to delete those listed files: [Y/N] ? "
 choice /n /c YN
-if "!errorlevel!"=="1" (
-    for /l %%a in (1 1 !database[#]!) do (
-        if exist "!database[%%a]!" (
-            >nul 2>&1 del /f /q "!database[%%a]!"
-            if exist "!database[%%a]!" %@ERROR%
+if "!ErrorLevel!"=="1" (
+    for /l %%A in (1 1 !database[#]!) do (
+        if exist "!database[%%A]!" (
+            >nul 2>&1 del /f /q "!database[%%A]!"
+            if exist "!database[%%A]!" %@ERROR%
         ) else (
             %@ERROR%
         )
     )
     if defined error call :EXIT 2
-    call :EXIT 1
+    call :EXIT 3
 )
 
 :EXIT
 echo.
 if not "%1"=="" (
-    if "%1"=="1" echo  ■ Successfully delete all files.
+    if "%1"=="1" echo  ■ This script must be "Run as administrator".
     if "%1"=="2" echo  ■ Some files could not be deleted.
+    if "%1"=="3" echo  ■ Successfully delete all files.
     echo.
 )
 <nul set /p= ■ Press {ANY KEY} to exit...
